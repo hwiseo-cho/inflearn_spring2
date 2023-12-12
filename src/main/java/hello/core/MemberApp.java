ba922657eaa81f0769a213f347fdf466663fe583
hwiseo <gnltj37@naver.com>
2023-12-12 21:33:48 +0900

    핵심 원리 이해1 ~ 핵심 원리 이해2-1

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemberService;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memeberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memeberService.join(member);
+
+        Member findMember = memeberService.findMember(1L);
+        System.out.println("new member: " + member.getName());
+        System.out.println("find member: " + findMember.getName());
+    }
+}
