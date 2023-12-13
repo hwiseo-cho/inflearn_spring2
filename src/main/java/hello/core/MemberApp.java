c4fdd7a1eb083ede7c8f8dfc9c21404f9ed23c57
hwiseo <gnltj37@naver.com>
2023-12-13 20:38:29 +0900

    핵심 원리 이해2-1 ~ 핵심 원리 이해2-9

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,15 +4,25 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memeberService = new MemberServiceImpl();
-        Member member = new Member(1L, "memberA", Grade.VIP);
-        memeberService.join(member);
+        // 순수 자바
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+        //MemberService memeberService = new MemberServiceImpl();
 
-        Member findMember = memeberService.findMember(1L);
+        // spring
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
         System.out.println("new member: " + member.getName());
         System.out.println("find member: " + findMember.getName());
     }
